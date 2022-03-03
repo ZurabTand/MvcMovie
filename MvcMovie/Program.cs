12704a5e114cbb0c4c64907203ddfaaae4f726aa
SuperDev <tandilashvilizurab@gmail.com>
2022-03-03 19:48:49 +0400

    Added new controller, EF Core packages, migrations, views by scaffolding

--- MvcMovie/Program.cs
+++ MvcMovie/Program.cs
@@ -1,4 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MvcMovie.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<MvcMovieContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("MvcMovieContext")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
